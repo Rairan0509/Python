4f39afa40f8007a95e8c6a222cce4fafe0d2facd
강병훈 <qudgns8598@gmail.com>
2026-09-14 00:54:03 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 23.96 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -0,0 +1,6 @@
+def solution(arr, queries):
+    for query in queries:
+        for i in range(len(arr)):
+            if query[0] <= i <= query[1] and i % query[2] == 0:
+                arr[i] += 1
+    return arr
