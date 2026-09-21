d8f7f58dbc18c85447926aaf2319064acdf45a0c
강병훈 <qudgns8598@gmail.com>
2026-09-21 17:27:54 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.05 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -1,5 +1,5 @@
 def solution(my_string, index_list):
     word = ""
-    for number in index_list:
-        word += my_string[number]
+    for idx in index_list:
+        word += my_string[idx]
     return word
