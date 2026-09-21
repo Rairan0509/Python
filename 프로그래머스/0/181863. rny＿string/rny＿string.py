2dfe7039cf735f0075929bd16ed11aa777fc7cf6
강병훈 <qudgns8598@gmail.com>
2026-09-21 16:55:05 +0900

    [level 0] Title: rny_string, Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.py
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -1,6 +1,2 @@
-def solution(rny_string: str):
-    idx = rny_string.find("m")
-    if idx != -1:
-        return rny_string.replace(rny_string[idx], "rn")
-    else:
-        return rny_string
+def solution(rny_string):
+    return rny_string.replace("m", "rn")
