09111d2d23801638ae8fbe98028c8cb4590865d7
강병훈 <qudgns8598@gmail.com>
2026-09-21 18:03:55 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,6 +1,11 @@
 def solution(num_list):
-    if num_list[len(num_list)-1] > num_list[len(num_list)-2]:
-        num_list.append(num_list[len(num_list)-1] - num_list[len(num_list)-2])
-        return num_list
-    num_list.append(num_list[len(num_list)-1]*2)
-    return num_list
+    num_list2 = []
+    for number in num_list:
+        num_list2.append(number)
+    
+    if num_list[-1] > num_list[-2]:
+        num_list2.append(num_list[-1]-num_list[-2])
+    else:
+        num_list2.append(num_list[-1]*2)
+
+    return num_list2
